bb91f69d582125e5ec771d5de8c95728fcd02188
sarveshrane2000 <sarveshrane2000@gmail.com>
2022-04-25 22:37:21 +0530

    Production changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
